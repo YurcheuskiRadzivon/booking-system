48a3b8a0e88fa6e9bfe79fa85241ccc740cea7ba
Yurcheuski Radzivon <yurcheuskiradzivon@gmail.com>
2025-12-15 23:28:05 +0300

    add start of project

---
+++ cmd/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/YurcheuskiRadzivon/booking-system/internal/config"
+	"github.com/YurcheuskiRadzivon/booking-system/internal/server"
+)
+
+func main() {
+	cfg, err := config.NewConfig()
+	if err != nil {
+		log.Fatalf("Config error: %s", err)
+	}
+
+	ctx, cancel := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer cancel()
+
+	run(cfg, ctx)
+}
+
+func run(cfg *config.Config, ctx context.Context) {
+	srv := server.New(cfg.HTTP.PORT)
+
+	srv.RegisterRoutes()
+
+	srv.Start()
+
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt, syscall.SIGTERM)
+
+	select {
+	case <-interrupt:
+		log.Println("Shutdown")
+
+	case err := <-srv.Notify():
+		log.Panicf("server: %s", err)
+	}
+
+	err := srv.Shutdown()
+	if err != nil {
+		log.Fatalf("server: %v", err)
+	}
+}
---
+++ internal/server/server.go
@@ -0,0 +1,81 @@
+package server
+
+import (
+	"encoding/json"
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+const (
+	_defaultAddr            = ":80"
+	_defaultReadTimeout     = 40 * time.Second
+	_defaultWriteTimeout    = 40 * time.Second
+	_defaultShutdownTimeout = 40 * time.Second
+)
+
+type Server struct {
+	app     *fiber.App
+	notify  chan error
+	address string
+}
+
+type Error struct {
+	Message string `json:"message" example:"message"`
+}
+
+func New(port string) *Server {
+	s := &Server{
+		app:     nil,
+		notify:  make(chan error, 1),
+		address: port,
+	}
+
+	app := fiber.New(fiber.Config{
+		BodyLimit:    30 * 1024 * 1024,
+		ReadTimeout:  _defaultReadTimeout,
+		WriteTimeout: _defaultWriteTimeout,
+		JSONEncoder:  json.Marshal,
+		JSONDecoder:  json.Unmarshal,
+	})
+
+	s.app = app
+
+	return s
+}
+
+func (s *Server) Start() {
+	go func() {
+		s.notify <- s.app.Listen(s.address)
+		close(s.notify)
+	}()
+}
+
+func (s *Server) RegisterRoutes() {
+	ui := s.app.Group("/ui")
+	{
+		ui.Get("/")
+	}
+
+	booking := s.app.Group("/booking")
+	{
+		booking.Get("/")
+	}
+
+	notification := s.app.Group("/notification")
+	{
+		notification.Get("/")
+	}
+}
+
+func (s *Server) Notify() <-chan error {
+	return s.notify
+}
+
+func (s *Server) Shutdown() error {
+	return s.app.ShutdownWithTimeout(_defaultShutdownTimeout)
+}
+
+func ErrorResponse(ctx *fiber.Ctx, code int, msg string) error {
+	return ctx.Status(code).JSON(Error{Message: msg})
+}
